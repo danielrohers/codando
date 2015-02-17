9b0bdada791eb8b54e2d8f9abaf90225888e7720
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-02-16 22:04:35 -0200

    add certificado

--- gulpfile.js
+++ gulpfile.js
@@ -12,23 +12,34 @@ gulp.task('minify', ['clean'], function() {
   gulp.src('src/assets/css/*.css')
     .pipe(minifyCSS())
     .pipe(gulp.dest('dist/assets/css'));
+
+  gulp.src('src/vendor/css/*.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist/vendor/css'));
 });
 
 gulp.task('uglify', ['clean'], function() {
   gulp.src('src/assets/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/assets/js'));
+
+  gulp.src('src/vendor/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/vendor/js'));
 });
 
 gulp.task('img', ['clean'], function () {
   gulp.src('src/assets/img/**')
     .pipe(gulp.dest('dist/assets/img'));
+
+  gulp.src('src/vendor/css/images/**')
+    .pipe(gulp.dest('dist/vendor/css/images'));
 });
 
 gulp.task('assets', ['minify', 'uglify', 'img']);
 
 gulp.task('html', ['clean'], function () {
-  gulp.src('src/*.html')
+  gulp.src(['src/*.html', 'src/*/*.html'])
     .pipe(minifyHTML())
     .pipe(gulp.dest('dist/'));
 });
